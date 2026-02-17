c056fc6359375aa16687af0bcbca19205ace2119
V-Ksenia <vilkseniakina@gmail.com>
2026-02-17 16:41:14 +0300

    static list of tours for demo

--- front_touragency/src/pages/Tour/TourPage.jsx
+++ front_touragency/src/pages/Tour/TourPage.jsx
@@ -4,7 +4,24 @@ import Advertising from "../../components/Advertising";
 import "./TourPage.css";
 
 const ToursPage = () => {
-  const [tours, setTours] = useState([]);
+  //const [tours, setTours] = useState([]);
+  const tours = [
+    {
+      _id: "1",
+      country: "Italy",
+      description: "Explore the beauty of Italy with us!",
+      duration: 2,
+      price: 1200,
+      img: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aXRhbHl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+    },
+    { _id: "2",
+      country: "Japan",
+      description: "Discover the wonders of Japan with us!",
+      duration: 3,
+      price: 1500,
+      img: "https://images.unsplash.com/photo-1549692520-acc6669e2f0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8amFwYW58ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+    }];
+
   const [filters, setFilters] = useState({
     country: "",
     minPrice: "",
@@ -12,23 +29,23 @@ const ToursPage = () => {
     sort: "",
   });
 
-  const fetchTours = async () => {
-    try {
-      const query = new URLSearchParams(
-        Object.entries(filters).filter(([_, value]) => value),
-      ).toString();
+  // const fetchTours = async () => {
+  //   try {
+  //     const query = new URLSearchParams(
+  //       Object.entries(filters).filter(([_, value]) => value),
+  //     ).toString();
 
-      const response = await fetch(`http://localhost:8080/api/tours?${query}`);
-      const data = await response.json();
-      setTours(data);
-    } catch (error) {
-      console.error("Error loading tours:", error);
-    }
-  };
+  //     const response = await fetch(`http://localhost:8080/api/tours?${query}`);
+  //     const data = await response.json();
+  //     setTours(data);
+  //   } catch (error) {
+  //     console.error("Error loading tours:", error);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchTours();
-  }, [filters]);
+  // useEffect(() => {
+  //   fetchTours();
+  // }, [filters]);
 
   const handleFilterChange = (e) => {
     setFilters({ ...filters, [e.target.name]: e.target.value });
